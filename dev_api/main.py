ad69a25b3f2b70a05eb2a8f9d06b6c4483ca07b6
Jan Eduardo <janeduardo@discente.ufg.br>
2022-08-21 14:01:17 -0300

    finish dev_api and add task_api

--- dev_api/main.py
+++ dev_api/main.py
@@ -1,10 +1,55 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import json
 
 app = Flask(__name__)
 
-@app.route('/dev')
-def desenvolvedor():
-    return jsonify({'nome': 'Rafael'})
+desenvolvedores = [
+    {
+        'id': 0,
+        'nome': 'Rafael',
+        'habilidades': ['Python', 'Flask']
+     },
+    {
+        'id': 1,
+        'nome': 'Pedro',
+        'habilidades': ['Python', 'Django']
+    }
+]
+
+# pega, altera e deleta desenvolvedores
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def desenvolvedor(id):
+    if request.method == 'GET':
+        try:
+            response = desenvolvedores[id]
+        except IndexError:
+            mensagem = 'Desenvolvedor de ID {} não existe'.format(id)
+            response = {'status': 'erro', 'mensagem': mensagem}
+        except Exception:
+            mensagem = 'Erro desconhecido. Procure o administrador da API'
+            response = {'status': 'erro', 'mensagem': mensagem}
+        return jsonify(response)
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        desenvolvedores[id] = dados
+        return jsonify(dados)
+    elif request.method == 'DELETE':
+        desenvolvedores.pop(id)
+        return jsonify({'status': 'sucesso', 'mensagem': 'Registro excluido'})
+
+
+# Lista e registra desenvolvedores
+@app.route('/dev/', methods=['POST', 'GET'])
+def lista_desenvolvedores():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        posicao = len(desenvolvedores)
+        dados['id'] = posicao
+        desenvolvedores.append(dados)
+        return jsonify(desenvolvedores[posicao])
+    elif request.method == 'GET':
+        return jsonify(desenvolvedores)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
